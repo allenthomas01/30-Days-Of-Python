82faaf5bed57cf0219b436d8bab11d687b341c8a
Allen <allenthomas204@gmail.com>
2023-07-27 19:33:21 +0530

    Completed day 05

--- 05_Day_Lists/05-exercise.py
+++ 05_Day_Lists/05-exercise.py
@@ -83,3 +83,121 @@ print(slice1)
 slice2 = it_companies[-3:]
 print(slice2)
 
+# 20
+middle = int((len(it_companies)-1)/2)
+middle_slice = it_companies[middle]
+print(middle_slice)
+
+# 21
+del it_companies[0]
+print(it_companies)
+
+# 22
+middle = int((len(it_companies)-1 )/2)
+del it_companies[middle:middle+2]
+print(it_companies)
+
+# 23
+it_companies.pop()
+print(it_companies)
+
+# 24
+it_companies.clear()
+print(it_companies)
+
+# 25
+del it_companies
+
+# 26
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+front_end.extend(back_end)
+print(front_end)
+
+# 27
+fullstack = front_end.copy()
+fullstack.append('python')
+fullstack.append('SQL')
+print(fullstack)
+
+# Exercise : level 2
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+ages.sort()
+print('Min age: {} , Max age: {}'.format(ages[0],ages[len(ages)-1]))
+ages.append(19)
+ages.append(26)
+ages.sort()
+middle = int((len(ages)-1)/2)
+print('Median age: {}'.format(ages[middle]))
+
+sum = 0
+for i,n in enumerate(ages):
+    sum = sum + n
+print('Average age is {}'.format(sum/len(ages)))
+
+maxValue = max(ages)
+minValue = min(ages)
+print('Range of ages is {}'.format(maxValue-minValue))
+
+average = int(sum / len(ages))
+print('(min - average) = {} \t (max-average)={}'.format(abs(minValue-average),maxValue-average))
+
+countries = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+# 1
+middle = int((len(countries)-1)/2)
+print(countries[middle])
+
+# 2
+length = len(countries)
+print(length)
+middle = int(((length-1)/2))
+if length%2==0:
+    print(countries[0:middle])
+else:
+    print(countries[0:middle+1] , countries[middle+1:])
+
+# 3
+first , second, third, *rest = countries
+print(first,second,third,rest)
+
+##########################3
+
+# OUTPUT
+
+# 7
+# first fourth seven
+# ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+# Number of companies is 7
+# First: Facebook , middle: IBM ,last: Amazon
+# ['Meta', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+# ['Meta', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon', 'TCS']
+# ['Meta', 'Google', 'Microsoft', 'Apple', 'Goldman Sachs', 'IBM', 'Oracle', 'Amazon', 'TCS']
+# ['META', 'Google', 'Microsoft', 'Apple', 'Goldman Sachs', 'IBM', 'Oracle', 'Amazon', 'TCS']
+# ['META', 'Google', 'Microsoft', 'Apple', 'Goldman Sachs', 'IBM', 'Oracle', 'Amazon', 'TCS', '#']
+# True
+# ['Amazon', 'Apple', 'Goldman Sachs', 'Google', 'IBM', 'META', 'Microsoft', 'Oracle', 'TCS']
+# ['TCS', 'Oracle', 'Microsoft', 'META', 'IBM', 'Google', 'Goldman Sachs', 'Apple', 'Amazon']
+# ['TCS', 'Oracle', 'Microsoft']
+# ['Goldman Sachs', 'Apple', 'Amazon']
+# IBM
+# ['Oracle', 'Microsoft', 'META', 'IBM', 'Google', 'Goldman Sachs', 'Apple', 'Amazon']
+# ['Oracle', 'Microsoft', 'META', 'Goldman Sachs', 'Apple', 'Amazon']
+# ['Oracle', 'Microsoft', 'META', 'Goldman Sachs', 'Apple']
+# []
+# ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB']
+# ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB', 'python', 'SQL']
+# Min age: 19 , Max age: 26
+# Median age: 24
+# Average age is 22.75
+# Range of ages is 7
+# (min - average) = 3 	 (max-average)=4
+# Finland
+# 7
+# ['China', 'Russia', 'USA', 'Finland'] ['Sweden', 'Norway', 'Denmark']
+# China Russia USA ['Finland', 'Sweden', 'Norway', 'Denmark']
+
+
+
+
+
